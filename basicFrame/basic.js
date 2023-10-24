2dab77abf9981e75537dfa519b8282c84f40e135
song5975 <badugi5655@naver.com>
2023-10-24 11:32:15 +0900

    20231024_1132

---
+++ basicFrame/basic.js
@@ -0,0 +1,11 @@
+// basic.js
+// JavaScript를 사용하여 button를 클릭하면 링크로 이동
+document.getElementById("link1").addEventListener("click", function () {
+    window.location.href = "https://www.naver.com"; // 원하는 링크 주소로 변경
+});
+document.getElementById("link2").addEventListener("click", function () {
+    window.location.href = "https://www.naver.com"; // 원하는 링크 주소로 변경
+});
+document.getElementById("link3").addEventListener("click", function () {
+    window.location.href = "https://www.naver.com"; // 원하는 링크 주소로 변경
+});
---
+++ reactwork/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    // javascript 주석
+    <div className="App">
+      {/* jsx 주석 */}
+        <div className='title'>React</div>
+    </div>
+  );
+}
+
+export default App;
